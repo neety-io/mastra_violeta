26c5bc76043a895b282ae6f2a5e9d4abb79039d4
vclar <violeta.clar@opendeusto>
2025-09-25 09:36:39 +0200

    cleaning

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,14 +1,12 @@
 import { Mastra } from '@mastra/core/mastra';
 import { PinoLogger } from '@mastra/loggers';
 import { LibSQLStore } from '@mastra/libsql';
-import { weatherWorkflow } from './workflows/weather-workflow';
-import { contentWorkflow, aiContentWorkflow, parallelAnalysisWorkflow, conditionalWorkflow } from './workflows/content-workflow';
 import { fuserAgent } from './agents/fuser-agent';
 import { fabricadosAgent } from './agents/fabricados';
 
 
 export const mastra = new Mastra({
-  workflows: { weatherWorkflow, contentWorkflow, aiContentWorkflow, parallelAnalysisWorkflow, conditionalWorkflow },
+  workflows: {},
   agents: { fuserAgent, fabricadosAgent},
   storage: new LibSQLStore({
     url: ":memory:",
